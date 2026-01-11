1e933dc54e36c076cb0248ae023635d5c73baf11
Paul McCombs <paul@allysonmccombs.com>
2026-01-11 14:24:25 -0800

    Added stylesheet to flask

--- flask_toy.py
+++ flask_toy.py
@@ -1,10 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+menu = """
+    1) Search by Artist
+    2) Search by Song Title
+    3) Search by Album Title
+    4) All details for song key
+    Q) Exit this demo
+    """
+
+test = """
+    Select a menu item to get some output.
+    """
+
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def index():
+    return render_template('index.html', menu_text=menu, output_text=test)
 
 if __name__ == "__main__":
     app.run(debug=True)
