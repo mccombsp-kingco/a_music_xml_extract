c6dcf6f33011fe0d98b8b94e3b3007b3689ac975
Paul <paul@allysonmccombs.com>
2026-02-17 22:22:58 -0800

    Added basic search functionality to flask_toy.py

--- flask_toy.py
+++ flask_toy.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import demo
 
 app = Flask(__name__)
@@ -8,7 +8,6 @@ menu = """
     2) Search by Song Title
     3) Search by Album Title
     4) All details for song key
-    Q) Exit this demo
     """
 
 test = demo.concat_results(demo.find_keys("1","Giants"))
@@ -16,6 +15,11 @@ test = demo.concat_results(demo.find_keys("1","Giants"))
 @app.route("/")
 def index():
     return render_template('index.html', menu_text=menu, output_text=test)
+
+@app.route("/results", methods=['POST'])
+def results():
+    test = demo.concat_results(demo.find_keys(request.form['option'],request.form['searchfor']))
+    return render_template('results.html', menu_text=menu, output_text=test)
 
 if __name__ == "__main__":
     app.run(debug=True)
